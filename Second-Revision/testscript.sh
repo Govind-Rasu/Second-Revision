afc320434c09a7d9f557c79e38ceef0db77274c5
Super User <root@DESKTOP-GBDOMP3.>
2024-12-27 21:10:21 +0530

    Files all deleted

--- Second-Revision/testscript.sh
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-#! bin/bash
-echo "Hello world from Local"
-echo "Hello world added from Rasu"
-echo "From Rasu2 and Rasu"
-echo "from Local"
-echo "From Rasu only"
-echo "Hello World from Rasu" 
