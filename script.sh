ee7bcaee607c551b3f7d073580810ffaad886b12
Govind-Rasu <99677947+Govind-Rasu@users.noreply.github.com>
2025-01-30 15:04:55 +0530

    Updated script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,5 @@
 echo "Script file added"
 echo "This is from Git Hub"
 echo "This is from Git Local"
-rm -rf /root/Second-Revision/Dust.txt
+mkdir /root/Second-Revision/Dust.txt
+echo "Dust file created successfully"
