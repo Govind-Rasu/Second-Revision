e8b0a0c908f5143f1b82a99c356dce0c3f4d85bc
Super User <root@DESKTOP-GBDOMP3.>
2025-01-30 19:57:38 +0530

    new script added

---
+++ Trigpipe2.sh
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Stage 1') {
+            steps {
+	    sh 'echo"Private IP of the server $(hostname -I) "'
+            }
+        }
+
+        stage('Stage 2') {
+            steps {
+	    sh 'echo "Public IP of the server $(curl ifconfig.me)"'
+            }
+        }
+    }
+}
