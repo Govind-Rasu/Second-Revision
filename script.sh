9911f57a7a21304b52fb968ee4b151cf2d3e05d6
Govind-Rasu <99677947+Govind-Rasu@users.noreply.github.com>
2025-01-30 14:22:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,4 @@
 echo "Script file added"
 echo "This is from Git Hub"
 echo "This is from Git Local"
-rm -rf /root/Second-Revision/test.txt
-mkdir /root/Second-Revision/Boost.txt
-  
+rm -rf /root/Second-Revision/Boost.txt
