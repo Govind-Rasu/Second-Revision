9c03cd5624895a1e094d39b207d33e931e114521
Govind-Rasu <99677947+Govind-Rasu@users.noreply.github.com>
2025-01-30 15:16:38 +0530

    Updated script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 echo "Script file added"
 echo "This is from Git Hub"
 echo "This is from Git Local"
-mkdir /root/Second-Revision/Rust.txt
-echo "Rust file created successfully"
+rm -rf /root/Second-Revision/Rust.txt
+echo "Rust file deleted successfully"
