0ad3f8fe8caab78c9f601ed5cbe4d2ae280172a0
Super User <root@DESKTOP-GBDOMP3.>
2025-01-30 14:45:12 +0530

    File modified

--- script.sh
+++ script.sh
@@ -2,5 +2,4 @@
 echo "Script file added"
 echo "This is from Git Hub"
 echo "This is from Git Local"
-rm -rf /root/Second-Revision/Boost.txt
-mkdir /root/Second-Revision/Dust.txt
+rm -rf /root/Second-Revision/Dust.txt
