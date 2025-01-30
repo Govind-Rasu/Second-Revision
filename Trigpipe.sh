a008c6d09f0e6f006ba30b879993f0d6875a609e
Govind-Rasu <99677947+Govind-Rasu@users.noreply.github.com>
2025-01-30 19:39:57 +0530

    Update Trigpipe.sh

--- Trigpipe.sh
+++ Trigpipe.sh
@@ -15,7 +15,7 @@ pipeline {
         stage('Stage 2') {
             steps {
 	     sh 'echo "Hi This is from Stage 2"'
-            sh 'mkdir /root/Second-Revision/helloman2.log'
+            sh 'mkdir /root/Second-Revision/helloman3.log'
 	     sh 'echo "The Private IP is $(hostname -I)"'    
             }
         }
