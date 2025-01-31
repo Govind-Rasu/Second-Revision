6a573cd1d5f7cfd13a768b79e9ebdea087c6a46d
Govind-Rasu <99677947+Govind-Rasu@users.noreply.github.com>
2025-01-31 15:58:56 +0530

    Updated serverdetails.sh

--- serverdetails.sh
+++ serverdetails.sh
@@ -1,4 +1,4 @@
-pipeline ()
+pipeline {
 
           agent any
 stages {
@@ -9,13 +9,14 @@ stages {
                  sh ‘echo “The name of the server is” ’
                  sh 'echo "$(hostname)"'
              }
+             }
         stage ('stage 2')
               {
               steps
                     {
                 sh ‘echo “The IP address of the server is” ’
                  sh 'echo "$(hostname -I)"'
-}
+              }
               }
                    }
 }
