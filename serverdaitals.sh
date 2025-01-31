48ce6c327f4d4669c3afde082d213afafa2267ed
Super User <root@DESKTOP-GBDOMP3.>
2025-01-31 15:47:16 +0530

    Files added and deleted

---
+++ serverdaitals.sh
@@ -0,0 +1,22 @@
+pipeline ()
+
+          agent any
+stages {
+       stage ('stage 1')
+                   {
+            steps
+             {
+                 sh ‘echo “The name of the server is” ’
+                 sh 'echo "$(hostname)"'
+             }
+        stage ('stage 2')
+              {
+              steps
+                    {
+                sh ‘echo “The IP address of the server is” ’
+                 sh 'echo "$(hostname -I)"'
+}
+              }
+                   }
+}
+
